9244ab13e0df16330d91bffaeed80da8a5d778d9
stodoriko <s.todorikos.gmail.com>
2022-01-21 19:06:38 +0300

    updated DashboardPage.java

--- src/test/java/ru/netology/web/page/DashboardPage.java
+++ src/test/java/ru/netology/web/page/DashboardPage.java
@@ -1,15 +1,39 @@
 package ru.netology.web.page;
 
+import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.SelenideElement;
+import lombok.val;
 
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
 
 public class DashboardPage {
 
     private SelenideElement heading = $("[data-test-id=dashboard]");
+    private ElementsCollection cards = $$(".list__item");
+    private final String balanceStart = "баланс: ";
+    private final String balanceFinish = " р.";
 
     public DashboardPage() {
         heading.shouldBe(visible);
     }
+
+    public int getFirstCardBalance() {
+        String text = cards.get(0).text();
+        return extractBalance(text);
+    }
+
+    public int getSecondCardBalance() {
+        String text = cards.get(1).text();
+        return extractBalance(text);
+    }
+
+    private int extractBalance(String text) {
+        val start = text.indexOf(balanceStart);
+        val finish = text.indexOf(balanceFinish);
+        val value = text.substring(start + balanceStart.length(), finish);
+        return Integer.parseInt(value);
+    }
+
 }
